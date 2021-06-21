529fe6ade3ff1e20ee112c4d3667ab6ee481a313
浜口綾 <aya.hamaguchi@consallink.co.jp>
2021-06-21 18:05:58 +0900

    投稿先、種類選択

---
+++ pages/post.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-col cols="6">
+      <v-card>
+        <v-img height="250" src="@/assets/kazetachinu035.jpg"></v-img>
+        <v-card-title> イラストを投稿する！</v-card-title>
+        <v-btn color="orange" elevation="2" rounded>全体に公開する</v-btn>
+        <v-btn color="purple" dark elevation="2" rounded>部室に投稿する</v-btn>
+      </v-card>
+    </v-col>
+    <v-col cols="6">
+      <v-card>
+        <v-img height="250" src="@/assets/mimi037.jpg"></v-img>
+        <v-card-title> 小説を投稿する！</v-card-title>
+        <v-btn color="orange" elevation="2" rounded>全体に公開する</v-btn>
+        <v-btn color="purple" dark elevation="2" rounded>部室に投稿する</v-btn>
+      </v-card>
+    </v-col>
+  </div>
+</template>
