7154fcefbfbb2f15fc9e69df289c5ab50622d44a
浜口綾 <aya.hamaguchi@consallink.co.jp>
2021-06-21 18:03:55 +0900

    自分の作品のみ表示、編集可能

---
+++ pages/display.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <v-container>
+      <v-row justify="center">
+        <v-col cols="12" lg="2">
+          <Cards />
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
