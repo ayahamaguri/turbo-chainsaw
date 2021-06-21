b14a3115cbfb8942b7a5fa984e9bddc6770ba8a9
浜口綾 <aya.hamaguchi@consallink.co.jp>
2021-06-21 11:38:28 +0900

    orangebtn

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,6 @@
       <div class="text-center">
         <logo />
         <vuetify-logo />
-        <v-btn>aaa</v-btn>
       </div>
       <v-card>
         <v-card-title class="headline">
@@ -75,6 +74,7 @@
         <v-card-actions>
           <v-spacer />
           <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
+          <v-btn color="orange">aaa</v-btn>
         </v-card-actions>
       </v-card>
     </v-col>
