5c585d6be4eb5c732f3fae77817b7f8a41bd155f
浜口綾 <aya.hamaguchi@consallink.co.jp>
2021-06-22 17:56:38 +0900

    小説投稿フォームコンポーネント

---
+++ components/NovelSheet.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-form>
+      <v-text-field :counter="30" label="タイトル">{{ ntitle }}</v-text-field>
+      <v-textarea label="本文">{{ text }}</v-textarea>
+      <v-textarea label="説明文など">{{ nexplanation }}</v-textarea>
+      <v-text-field :counter="30" label="タグ">{{ ntags }}</v-text-field>
+    </v-form>
+  </div>
+</template>
+<script type="ts">
+import { defineComponent} from '@nuxtjs/composition-api'
+export default defineComponent({
+    // propsで受け取る属性を定義
+  name: 'NovelData',
+  props: {
+      ntitle:{
+          type: String,
+          default: '',
+      },
+      text:{
+          type: String,
+          default: '',
+      },
+      nexplanation:{
+          type: String,
+          default: '',
+      },
+      ntags:{
+          type: String,
+          default: '',
+      },
+  },
+    setup(){
+    }
+})
+</script>
