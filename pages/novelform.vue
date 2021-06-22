74221ee008b4de77cba530116833e7aa92e1be0c
浜口綾 <aya.hamaguchi@consallink.co.jp>
2021-06-22 18:16:33 +0900

    小説・画像投稿フォーム親画面追加

---
+++ pages/novelform.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <novel-sheet></novel-sheet>
+    <v-btn href="/display">投稿する！<v-icon>mdi-upload</v-icon> </v-btn>
+  </div>
+</template>
+<script type="ts">
+import { defineComponent, ref} from '@nuxtjs/composition-api'
+import NovelSheet from '~/components/NovelSheet.vue'
+export default defineComponent({
+  components: { NovelSheet },
+    setup(){
+      const NovelData = ref([
+        {
+         ntitle:'',
+         text:'',
+         nexplanation:'',
+         ntags:'',
+        }
+      ])
+      return{
+        NovelData
+      }
+    }
+})
+</script>
