a7086fe49dce3e5996c2aa11f32382f59fdfefe9
浜口綾 <aya.hamaguchi@consallink.co.jp>
2021-06-22 17:59:46 +0900

    画像投稿フォーム（親）

---
+++ pages/imgform.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <img-sheet></img-sheet>
+    <v-btn href="/display">投稿する！<v-icon>mdi-upload</v-icon> </v-btn>
+  </div>
+</template>
+<script type="ts">
+import { defineComponent, ref} from '@nuxtjs/composition-api'
+import ImgSheet from '~/components/ImgSheet.vue'
+export default defineComponent({
+  components: { ImgSheet },
+    setup(){
+      const ImgData = ref([
+        {
+         ititle:'',
+         img:'',
+         iexplanation:'',
+         itags:'',
+        }
+      ])
+      return {
+        ImgData
+      }
+    }
+})
+</script>
