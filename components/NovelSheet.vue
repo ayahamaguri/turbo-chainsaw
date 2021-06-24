b6678589ec99eede0939313289d37d7c6021efe0
浜口綾 <aya.hamaguchi@consallink.co.jp>
2021-06-24 11:01:58 +0900

    formにid名追加、 サンプルインクリメント機能追加

--- components/NovelSheet.vue
+++ components/NovelSheet.vue
@@ -1,37 +1,18 @@
 <template>
-  <div>
+  <div id="novelsheet">
     <v-form>
-      <v-text-field :counter="30" label="タイトル">{{ ntitle }}</v-text-field>
-      <v-textarea label="本文">{{ text }}</v-textarea>
-      <v-textarea label="説明文など">{{ nexplanation }}</v-textarea>
-      <v-text-field :counter="30" label="タグ">{{ ntags }}</v-text-field>
+      <v-text-field :counter="30" label="タイトル"></v-text-field>
+      <v-textarea label="本文"></v-textarea>
+      <v-textarea label="説明文など"></v-textarea>
+      <v-text-field :counter="30" label="タグ"></v-text-field>
+      <v-text-field label="サンプル数字"></v-text-field>
+      <v-btn @click="increment">+</v-btn>
     </v-form>
   </div>
 </template>
 <script type="ts">
 import { defineComponent} from '@nuxtjs/composition-api'
 export default defineComponent({
-    // propsで受け取る属性を定義
-  name: 'NovelData',
-  props: {
-      ntitle:{
-          type: String,
-          default: '',
-      },
-      text:{
-          type: String,
-          default: '',
-      },
-      nexplanation:{
-          type: String,
-          default: '',
-      },
-      ntags:{
-          type: String,
-          default: '',
-      },
-  },
-    setup(){
-    }
+
 })
 </script>
