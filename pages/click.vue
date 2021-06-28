4c1bc05d4143bd24972e0d1e58ddba2856059825
浜口綾 <aya.hamaguchi@consallink.co.jp>
2021-06-28 09:49:50 +0900

    Vueのイベントについて

---
+++ pages/click.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-btn @click="log">@click="log"</v-btn>
+        <v-btn @click="(event) => log(event)"
+          >@click="(event) => log(event)"</v-btn
+        >
+        <v-btn @click="log('aaa')">@click="log('aaa')"</v-btn>
+        <v-btn @click="() => log('aaa')">@click="() => log('aaa')"</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+export default defineComponent({
+  setup() {
+    return {
+      log: console.log,
+    }
+  },
+})
+</script>
